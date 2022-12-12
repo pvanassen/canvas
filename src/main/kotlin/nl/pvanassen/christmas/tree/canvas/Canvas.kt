5805e0d9d168ba40bab6ae5bbddf04f930216cc4
Paul van Assen <source@pvanassen.nl>
2022-12-12 08:31:11 +0100

    Merging with common

--- src/main/kotlin/nl/pvanassen/christmas/tree/canvas/Canvas.kt
+++ src/main/kotlin/nl/pvanassen/christmas/tree/canvas/Canvas.kt
@@ -2,13 +2,15 @@ package nl.pvanassen.christmas.tree.canvas
 
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
+import kotlin.math.max
 
 /**
  * Canvas class to draw pixels with
  */
-public class Canvas(private val strips:Int, private val ledsPerStrip: Int) {
+public class Canvas(private val strips: Int, private val ledsPerStrip: Int) {
 
-    private val mask: BufferedImage = ImageIO.read(Canvas::class.java.getResourceAsStream("/mask-$strips-$ledsPerStrip.png"))
+    private val mask: BufferedImage =
+        ImageIO.read(Canvas::class.java.getResourceAsStream("/mask-$strips-$ledsPerStrip.png"))
     val canvas: BufferedImage
     private val positions: Positions
 
@@ -16,23 +18,25 @@ public class Canvas(private val strips:Int, private val ledsPerStrip: Int) {
         canvas = BufferedImage(mask.width, mask.height, BufferedImage.TYPE_INT_RGB)
         // getRGB returns int in TYPE_INT_ARGB
         val positionsList = (0 until mask.width)
-                .flatMap { x -> (0 until mask.height)
-                        .map { y -> Triple(x, y, mask.getRGB(x, y) and 0xFFFFFF) } }
-                .filter { it.third != 0 }
-                .map { Position(it.third shr 16 and 0xFF, it.third and 0xFF, it.first, it.second) }
+            .flatMap { x ->
+                (0 until mask.height)
+                    .map { y -> Triple(x, y, mask.getRGB(x, y) and 0xFFFFFF) }
+            }
+            .filter { it.third != 0 }
+            .map { Position(it.third shr 16 and 0xFF, it.third and 0xFF, it.first, it.second) }
         positions = Positions(positionsList, strips, ledsPerStrip)
     }
 
-    fun getValues():ByteArray {
+    fun getValues(): ByteArray {
         val values = ByteArray((strips * ledsPerStrip * 3))
         for (strip in 0 until strips) {
             for (pixel in 0 until ledsPerStrip) {
                 val x = positions.getX(strip, pixel)
                 val y = positions.getY(strip, pixel)
                 val color = canvas.getRGB(x, y)
-                val red:Byte = (color shr 16 and 0xFF).toByte()
-                val green:Byte = (color shr 8 and 0xFF).toByte()
-                val blue:Byte = (color and 0xFF).toByte()
+                val red: Byte = (color shr 16 and 0xFF).toByte()
+                val green: Byte = (color shr 8 and 0xFF).toByte()
+                val blue: Byte = (color and 0xFF).toByte()
                 val base = (strip * ledsPerStrip * 3) + pixel * 3
                 values[base] = red
                 values[base + 1] = green
@@ -42,51 +46,57 @@ public class Canvas(private val strips:Int, private val ledsPerStrip: Int) {
         return values
     }
 
-    fun setValue(strip:Int, pixel:Int, color:Int) {
+    fun setValue(strip: Int, pixel: Int, color: Int) {
         val x = positions.getX(strip, pixel)
         val y = positions.getY(strip, pixel)
         canvas.setRGB(x, y, color)
     }
 
-    fun setImage(offsetX:Int, offsetY:Int, image:BufferedImage, outOfBoundsBlack: Boolean = true) {
+    fun setImage(offsetX: Int, offsetY: Int, image: BufferedImage, outOfBoundsBlack: Boolean = true) {
         for (strip in 0 until strips) {
             for (pixel in 0 until ledsPerStrip) {
                 val x = positions.getX(strip, pixel) + offsetX
                 val y = positions.getY(strip, pixel) + offsetY
                 val color = if ((x >= image.width || y >= image.height) && outOfBoundsBlack) {
                     0
-                }
-                else {
-                    image.getRGB(Math.max(0, x), Math.max(0, y))
+                } else {
+                    image.getRGB(max(0, x), max(0, y))
                 }
                 canvas.setRGB(positions.getX(strip, pixel), positions.getY(strip, pixel), color)
             }
         }
     }
 
-    class Positions(private val positions:List<Position>, strips: Int, pixels:Int) {
+    private class Positions(private val positions: List<Position>, strips: Int, pixels: Int) {
 
-        private val raster:Array<Array<Position>>
+        private val raster: Array<Array<Position>>
 
         init {
-            raster = (0..strips).map { strip -> positions
-                    .filter { it -> it.strip == strip } }
-                    .map { stripPositions -> stripPositions
-                            .filter { it.pixel in (0..pixels) }
-                            .sortedBy { it.pixel }
-                            .toTypedArray()
-                    }.toTypedArray()
+            raster = (0..strips).map { strip ->
+                positions
+                    .filter { it -> it.strip == strip }
+            }
+                .map { stripPositions ->
+                    stripPositions
+                        .filter { it.pixel in (0..pixels) }
+                        .sortedBy { it.pixel }
+                        .toTypedArray()
+                }.toTypedArray()
         }
 
-        fun getX(strip:Int, pixel:Int):Int {
+        fun getX(strip: Int, pixel: Int): Int {
             return raster[strip][pixel].x
         }
 
-        fun getY(strip:Int, pixel:Int):Int {
+        fun getY(strip: Int, pixel: Int): Int {
             return raster[strip][pixel].y
+        }
+
+        override fun toString(): String {
+            return raster.toString()
         }
 
     }
 
-    class Position(val strip: Int, val pixel:Int, val x:Int, val y:Int)
+    private data class Position(val strip: Int, val pixel: Int, val x: Int, val y: Int)
 }
--- src/test/kotlin/nl/pvanassen/christmas/tree/canvas/CanvasTest.kt
+++ src/test/kotlin/nl/pvanassen/christmas/tree/canvas/CanvasTest.kt
@@ -1,12 +1,11 @@
 package nl.pvanassen.christmas.tree.canvas
 
-import assertk.assert
-import assertk.assertions.isEqualTo
-import assertk.assertions.isNotEmpty
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.awt.Color
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
+
 
 internal class CanvasTest {
     private val canvas = Canvas(16, 60)
@@ -16,8 +15,8 @@ internal class CanvasTest {
         val mask: BufferedImage = ImageIO.read(Canvas::class.java.getResourceAsStream("/test-mask1-16-60.png"))
         canvas.canvas.graphics.drawImage(mask, 0, 0, null)
         val lightStrips = canvas.getValues()
-        assert(lightStrips).isNotEmpty()
-        lightStrips.forEach { assert(it).isEqualTo(0.toByte()) }
+        assertThat(lightStrips).isNotEmpty
+        lightStrips.forEach { assertThat(it).isEqualTo(0.toByte()) }
     }
 
     @Test
@@ -25,11 +24,11 @@ internal class CanvasTest {
         val mask: BufferedImage = ImageIO.read(Canvas::class.java.getResourceAsStream("/test-mask2-16-60.png"))
         canvas.canvas.graphics.drawImage(mask, 0, 0, null)
         val lightStrips = canvas.getValues()
-        (0 until lightStrips.size step 3).forEach {
+        (lightStrips.indices step 3).forEach {
             val red = lightStrips[it].toInt() and 0xFF
             val green = lightStrips[it + 1].toInt() and 0xFF
             val blue = lightStrips[it + 2].toInt() and 0xFF
-            assert(Color(red, green, blue).red, "Pos: $it").isEqualTo(255)
+            assertThat(Color(red, green, blue).red).isEqualTo(255).overridingErrorMessage("Pos: $it")
         }
 
     }
